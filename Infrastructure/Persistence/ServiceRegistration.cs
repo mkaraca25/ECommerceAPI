21a6431d08f1d9c391c854ba5fc4f03beff04273
mkaraca25 <61596919+mkaraca25@users.noreply.github.com>
2022-02-28 16:15:12 +0300

    GetProduct listed

---
+++ Infrastructure/Persistence/ServiceRegistration.cs
@@ -0,0 +1,20 @@
+﻿using Application.Abstractions;
+using Microsoft.Extensions.DependencyInjection;
+using Persistence.Concretes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistence
+{
+    public static class ServiceRegistration
+    {
+        public static void AddPersistenceServices(this IServiceCollection services)
+        {
+            services.AddSingleton<IProductService, ProductService>();
+
+        }
+    }
+}
--- Presentation/API/Program.cs
+++ Presentation/API/Program.cs
@@ -1,7 +1,9 @@
+using Persistence;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddPersistenceServices();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();

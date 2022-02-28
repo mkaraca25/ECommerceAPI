822328ac3b5ae6db2e3122ef2dfe3969de59ba6e
mkaraca25 <61596919+mkaraca25@users.noreply.github.com>
2022-02-28 13:41:26 +0300

    28Sub2022

---
+++ Core/Domain/Entities/Product.cs
@@ -0,0 +1,16 @@
+﻿using Domain.Entities.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Product:BaseEntity
+    {
+        public string Name { get; set; }
+        public int Stock { get; set; }
+        public long Price { get; set; }
+    }
+}

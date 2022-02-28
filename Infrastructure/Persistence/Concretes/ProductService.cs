1d0f0a62721621aa611d0f3f496bfa4c958972c9
mkaraca25 <61596919+mkaraca25@users.noreply.github.com>
2022-02-28 14:01:31 +0300

    28Sub2022

---
+++ Infrastructure/Persistence/Concretes/ProductService.cs
@@ -0,0 +1,24 @@
+﻿using Application.Abstractions;
+using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistence.Concretes
+{
+    public class ProductService : IProductService
+    {
+        public List<Product> GetProducts()
+            => new()
+            {
+                new() { Id = Guid.NewGuid(), Name ="Prroduct 1",Price =100,Stock=15},
+                new() { Id = Guid.NewGuid(),Name ="Product 2",Price=150,Stock=15},
+                new() { Id = Guid.NewGuid(), Name = "Product 3", Price = 250, Stock = 15 },
+                new() { Id = Guid.NewGuid(), Name = "Product 4", Price = 50, Stock = 15 },
+                new() { Id = Guid.NewGuid(), Name = "Product 5", Price = 75, Stock = 15 }
+            };
+        
+    }
+}

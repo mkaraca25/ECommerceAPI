ba9ca8d8a67ae794e5b89bad185cf06a14b4ab1c
mkaraca25 <61596919+mkaraca25@users.noreply.github.com>
2022-03-11 15:18:29 +0300

    7bolumEnd

--- Infrastructure/Persistence/Repositories/ReadRepository.cs
+++ Infrastructure/Persistence/Repositories/ReadRepository.cs
@@ -21,15 +21,37 @@ namespace Persistence.Repositories
         }
         public DbSet<T> Table => _context.Set<T>();
 
-        public IQueryable<T> GetAll()
-        => Table;
+        public IQueryable<T> GetAll(bool tracking = true)
+        {
+            var query = Table.AsNoTracking();
+            if (!tracking)
+                query = query.AsNoTracking();
+            return query;
+        }
 
-        public IQueryable<T> GetWhere(Expression<Func<T, bool>> method)
-       =>Table.Where(method);
-        public async Task<T> GetSingleAsync(Expression<Func<T, bool>> method)
-        =>await Table.FirstOrDefaultAsync(method);
+        public IQueryable<T> GetWhere(Expression<Func<T, bool>> method, bool tracking = true)
+        {
+            var query = Table.Where(method);
+            if (!tracking)
+                query = Table.AsNoTracking();
+            return query; 
+        }
+        public async Task<T> GetSingleAsync(Expression<Func<T, bool>> method, bool tracking = true)
+        {
+            var query = Table.AsQueryable();
+            if (!tracking)
+                query = Table.AsNoTracking();
+            return await query.FirstOrDefaultAsync(method);
+        }
 
-        public async Task<T> GetByIdAsync(string id)
-        =>await Table.SingleOrDefaultAsync(data=>data.Id == Guid.Parse(id));
+        public async Task<T> GetByIdAsync(string id, bool tracking = true)
+        //=>await Table.SingleOrDefaultAsync(data=>data.Id == Guid.Parse(id));
+        {
+            var query = Table.AsQueryable();
+            if (!tracking)
+                query = query.AsNoTracking();
+            return await query.FirstOrDefaultAsync(data=>data.Id==Guid.Parse(id));
+
+        }
     }
 }
--- Presentation/API/Controllers/ProductsController.cs
+++ Presentation/API/Controllers/ProductsController.cs
@@ -1,4 +1,5 @@
 ﻿using Application.Repositories;
+using Domain.Entities;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -17,15 +18,25 @@ namespace API.Controllers
             _productWriteRepository = productWriteRepository;
         }
         [HttpGet]
-        public async void Get()
+        public async Task Get()
         {
-            await _productWriteRepository.AddRangeAsync(new() 
-            { 
-                new() { Id = Guid.NewGuid(),Name="Product 1",Price=100,CreatedDate=DateTime.UtcNow,Stock=10 },
+
+            /*await _productWriteRepository.AddRangeAsync(new()
+            {
+                new() { Id = Guid.NewGuid(), Name = "Product 1", Price = 100, CreatedDate = DateTime.UtcNow, Stock = 10 },
                 new() { Id = Guid.NewGuid(), Name = "Product 2", Price = 25, CreatedDate = DateTime.UtcNow, Stock = 15 },
                 new() { Id = Guid.NewGuid(), Name = "Product 3", Price = 150, CreatedDate = DateTime.UtcNow, Stock = 20 },
             });
-            var count=await _productWriteRepository.SaveAsync();
+            var count = await _productWriteRepository.SaveAsync();*/
+            /*Product p = await _productReadRepository.GetByIdAsync("fc0ad718-a5c6-4f57-a4f9-7d00facb7bfd",false);//takip edilmek isteniyorsa false silinir.
+            p.Name = "Mehmet";
+            await _productWriteRepository.SaveAsync();*/
+        }
+        [HttpGet("{id}")]
+        public async Task<ActionResult> Get(string id)
+        {
+            Product product = await _productReadRepository.GetByIdAsync(id);
+            return Ok(product);
         }
     }
 }
